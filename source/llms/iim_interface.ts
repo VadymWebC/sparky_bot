b24b2341302ac8913015786ae61109c21100cdd2
vadym <vadymcdevelweb@gmail.com>
2025-04-12 19:47:53 +0300

    updating the llm interface

--- source/llms/iim_interface.ts
+++ source/llms/iim_interface.ts
@@ -2,5 +2,5 @@ import { Module } from "../module/module_interface"
 import { Option } from "../types/option"
 
 export interface LargeLanguageModel extends Module {
-	generate(): Promise<Option<string>>
+	generate(promt: string): Promise<Option<string>>
 }
