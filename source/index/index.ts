5ac7f0229a82e870dcea2b4e29faf701d98d5e9c
vadym <vadymcdevelweb@gmail.com>
2025-04-12 19:55:10 +0300

    update

--- source/index/index.ts
+++ source/index/index.ts
@@ -1,5 +1,11 @@
-function main(): void {
+import { LargeLanguageModel } from "../llms/iim_interface"
+import { LargeLanguageModelOpenai } from "../llms/llm_openai"
+
+async function main(): Promise<void> {
 	console.log("OMG IT WORKS!!!")
+
+	const llm: LargeLanguageModel = new LargeLanguageModelOpenai()
+	await llm.generate("Hey what's up")
 
 	while (true) {}
 }
