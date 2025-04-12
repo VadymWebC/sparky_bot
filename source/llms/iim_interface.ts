8727dd5f8865f751eacd58f5360215159f1be1eb
vadym <vadymcdevelweb@gmail.com>
2025-04-12 19:31:22 +0300

    update

--- source/llms/iim_interface.ts
+++ source/llms/iim_interface.ts
@@ -1,5 +1,6 @@
 import { Module } from "../module/module_interface"
+import { Option } from "../types/option"
 
 export interface LargeLanguageModel extends Module {
-	generate(): Promise<void>
+	generate(): Promise<Option<string>>
 }
---
+++ source/llms/llm_openai.ts
@@ -0,0 +1,25 @@
+import Openai from "openai"
+import { Option } from "../types/option"
+import { LargeLanguageModel } from "./iim_interface"
+
+export class LargeLangualeModelOpenai implements LargeLanguageModel {
+	#client: Openai
+	constructor() {
+		this.#client = new Openai({
+			organization: null,
+		})
+	}
+	init(): Promise<void> {
+		//
+	}
+	free(): Promise<void> {
+		//
+	}
+	generate(): Promise<Option<string>> {
+		const completion = await openai.chat.completions.create({
+			messages: [{ role: "system", content: "You are a helpful assistant." }],
+			model: "gpt-3.5-turbo",
+		})
+		console.log(completion.choices[0])
+	}
+}
