ee1f62d4692289452f5239d9ecd48f74ab94f72e
vadym <vadymcdevelweb@gmail.com>
2025-04-12 22:10:00 +0300

    update

--- source/index/index.ts
+++ source/index/index.ts
@@ -1,8 +1,11 @@
+import { getAuth } from "../auth/get_auth"
 import { LargeLanguageModel } from "../llms/iim_interface"
 import { LargeLanguageModelOpenai } from "../llms/llm_openai"
 
 async function main(): Promise<void> {
 	console.log("OMG IT WORKS!!!")
+	const auth = getAuth()
+	console.log(auth)
 
 	const llm: LargeLanguageModel = new LargeLanguageModelOpenai()
 	await llm.generate("Hey what's up")
