4a510222feba3682d18311edfc7c743df91362b6
vadym <vadymcdevelweb@gmail.com>
2025-04-12 21:40:09 +0300

    update

---
+++ source/auth/get_auth.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs"
+import * as path from "path"
+
+export function getAuth(): Record<string, string> {
+	const PATH = path.join(process.cwd(), ".env")
+	const file_content: string = fs.readFileSync(PATH, { encoding: "utf8" })
+
+	const file_lines: string[] = file_content.split(/\r\n|\n/g)
+
+	let result: Record<string, string>
+
+	for (let line of file_lines) {
+		const split_line = line.split("")
+	}
+}
